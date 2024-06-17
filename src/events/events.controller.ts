a8797bdea5524a01b1120680b3080fa915354125
Michael Rodrigues <run.michael97@gmail.com>
2024-06-16 23:30:43 -0300

    feat: partner can create Events

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,51 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, UseInterceptors } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { CreateEventDto } from './dto/create-event.dto';
-import { UpdateEventDto } from './dto/update-event.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
-import { UsersService } from 'src/auth/users/users.service';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { TenantInterceptor } from 'src/tenant/tenant.interceptor';
 
+@UseInterceptors(TenantInterceptor)
 @UseGuards(AuthGuard)
 @Controller('events')
 export class EventsController {
   constructor(
-    private readonly eventsService: EventsService,
-    private userService: UsersService,
-    private prismaService: PrismaService,
-  ) {}
+    private readonly eventsService: EventsService) {}
 
   @Post()
-  async create(@Body() createEventDto: CreateEventDto, @Req() req: any) {
-    const user = await this.userService.findOne(req.user.id); 
-    if(!user){
-      throw new Error('User not found');
-    }
-    const partnerUser = await this.prismaService.partnerUser.findOne({
-      where: {
-        userId: user.id
-      }
-    })
-    return this.eventsService.create(partnerUser.partnerId);
+  async create(@Body() createEventDto: CreateEventDto) {
+    return this.eventsService.create(createEventDto);
   }
-
-  // @Get()
-  // findAll() {
-  //   return this.eventsService.findAll();
-  // }
-
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.eventsService.findOne(+id);
-  // }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
-  //   return this.eventsService.update(+id, updateEventDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.eventsService.remove(+id);
-  // }
 }
