a5d12864519abae51d6429a7a014562ce3d3017c
Michael Rodrigues <run.michael97@gmail.com>
2024-06-16 22:27:40 -0300

    feat: method for create partners with useGuard

--- src/partners/partners.controller.ts
+++ src/partners/partners.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { PartnersService } from './partners.service';
 import { CreatePartnerDto } from './dto/create-partner.dto';
-import { UpdatePartnerDto } from './dto/update-partner.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('partners')
 export class PartnersController {
   constructor(private readonly partnersService: PartnersService) {}
 
   @Post()
-  create(@Body() createPartnerDto: CreatePartnerDto) {
-    return this.partnersService.create(createPartnerDto);
+  create(@Body() createPartnerDto: CreatePartnerDto, @Req() req: any) {
+    return this.partnersService.create({
+      ...createPartnerDto,
+      userId: req.user.id,
+    });
   }
 
   @Get()
@@ -17,18 +21,18 @@ export class PartnersController {
     return this.partnersService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.partnersService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.partnersService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePartnerDto: UpdatePartnerDto) {
-    return this.partnersService.update(+id, updatePartnerDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updatePartnerDto: UpdatePartnerDto) {
+  //   return this.partnersService.update(+id, updatePartnerDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.partnersService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.partnersService.remove(+id);
+  // }
 }
--- src/partners/partners.service.ts
+++ src/partners/partners.service.ts
@@ -1,29 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePartnerDto } from './dto/create-partner.dto';
-import { UpdatePartnerDto } from './dto/update-partner.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class PartnersService {
   constructor(private prismaService: PrismaService) {}
 
-  create(createPartnerDto: CreatePartnerDto) {
-    return 'This action adds a new partner';
-  }
+  async create(createPartnerDto: CreatePartnerDto & {userId: number}) {
+    const partner = await this.prismaService.$transaction(async (prisma) => {
+      const partner = await prisma.partner.create({
+      data: {name: createPartnerDto.name},
+    });
+    await prisma.partnerUser.create({
+      data: {
+        userId: createPartnerDto.userId,
+        partnerId: partner.id,
+      },
+    });
 
+    return partner;
+    });
+  return partner;
+  }
   findAll() {
-    return `This action returns all partners`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} partner`;
-  }
-
-  update(id: number, updatePartnerDto: UpdatePartnerDto) {
-    return `This action updates a #${id} partner`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} partner`;
+    return this.prismaService.partner.findMany();
   }
 }
